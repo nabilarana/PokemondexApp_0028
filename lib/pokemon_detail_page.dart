a06b23c6292f4c66f559ebd22591454be2457321
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2025-12-11 13:58:59 +0700

    update

--- lib/pokemon_detail_page.dart
+++ lib/pokemon_detail_page.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex_app/model/pokemon.dart';
 
 class PokemonDetailPage extends StatelessWidget {
-  final String imageUrl;
-  final String name;
-  final String number;
+  final Pokemon pokemon;
 
-  const PokemonDetailPage({
-    Key? key,
-    required this.imageUrl,
-    required this.name,
-    required this.number,
-  }) : super(key: key);
+  const PokemonDetailPage({Key? key, required this.pokemon}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      appBar: AppBar(title: Text(pokemon.name)),
+      body: Center(child: Text("Detail Page for ${pokemon.name}")),
+    );
   }
 }
